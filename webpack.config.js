0838098c2030b793677ae9e4f35ccd9a0d4aee54
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-10-09 02:54:45 -0400

    initialized webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
+            // plugins: ['@babel/plugin-transform-runtime']
           }
         }
       },
