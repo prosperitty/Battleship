0dd1af1f078707561a33a8e674bb46224e6db515
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-10-24 03:02:22 -0400

    tested toggleTurn function

---
+++ src/player.test.js
@@ -0,0 +1,7 @@
+import { Player } from "./player";
+
+test('toggle players turn', () => {
+  const player1 = Player('player1');
+  expect(player1.toggleTurn()).toBeTruthy();
+  expect(player1.toggleTurn()).toBeFalsy();
+});
