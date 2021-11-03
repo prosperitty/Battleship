1c13254480d144a8f0b8cfb4dd7ca570c62908c4
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-11-03 02:46:15 -0400

    fixed bug where players turns were not being toggled

--- src/player.js
+++ src/player.js
@@ -3,22 +3,33 @@ export { Player };
 
 function Player(name) {
   let turn = false;
-  const gameboard = Gameboard();
+  
+  function toggleTurn() {
+    this.turn ? this.turn = false : this.turn = true;
+  }
 
-  const toggleTurn = () => !turn ? turn = true : turn = false;
-  const getName = () => name;
-
-  const attack = (enemyBoard) => {
-    if (turn) {
+  function attack(enemyBoard, row, column) {
+    if (this.turn && name === 'player') {
+      if (enemyBoard.receiveAttack(row, column)) {
+        console.log(row, column);
+        this.toggleTurn();
+      }
+    } else if (this.turn && name === 'computer') {
       let randomCoordinate = [randomNum(), randomNum()];
       enemyBoard.receiveAttack(...randomCoordinate);
       this.toggleTurn();
+      return findReducedIndex(enemyBoard, ...randomCoordinate);
     }
-  };
+  }
+
+  function findReducedIndex(enemyBoard, x, y) {
+    let flatBoard = enemyBoard.board.flat();
+    return flatBoard.indexOf(enemyBoard.board[x][y]);
+  }
 
   function randomNum() {
     return Math.round(Math.random() * (0 - 10) + 10);
   }
 
-  return { gameboard, attack, toggleTurn, getName };
+  return { attack, turn, toggleTurn };
 }
