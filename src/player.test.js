2f5a3e222fb229db20bc6d8dc5ce49fe9c03a399
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-11-03 02:54:42 -0400

    updated test to properly expect boolean values

--- src/player.test.js
+++ src/player.test.js
@@ -2,6 +2,8 @@ import { Player } from './player';
 
 test('toggle players turn', () => {
   const player1 = Player('player1');
-  expect(player1.toggleTurn()).toBeTruthy();
-  expect(player1.toggleTurn()).toBeFalsy();
+  player1.toggleTurn();
+  expect(player1.turn).toBeTruthy();
+  player1.toggleTurn();
+  expect(player1.turn).toBeFalsy();
 });
