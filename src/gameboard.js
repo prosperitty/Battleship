ce0d86efd075215e62a76a738ed65e6a6efc4d13
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-11-16 02:51:53 -0500

    added random ship allocation and placement

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,7 @@
 import { Ship } from './ship';
 export { Gameboard };
 
-function Gameboard(uiBoard) {
+function Gameboard() {
   const carrier = Ship('carrier', 5);
   const battleship = Ship('battleship', 4);
   const cruiser = Ship('cruiser', 3);
@@ -9,44 +9,65 @@ function Gameboard(uiBoard) {
   const destroyer2 = Ship('destroyer2', 2);
   const submarine1 = Ship('submarine1', 1);
   const submarine2 = Ship('submarine2', 1);
+  const ships = [
+    carrier,
+    battleship,
+    cruiser,
+    destroyer1,
+    destroyer2,
+    submarine1,
+    submarine2,
+  ];
   let board = [[], [], [], [], [], [], [], [], [], []];
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      board[i][j] = {
-        coordinate: [i, j],
-        ship: null,
-        isHit: false,
-        isMiss: false,
-      };
+  function initializeCells() {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        board[i][j] = {
+          coordinate: [i, j],
+          ship: null,
+          isHit: false,
+          isMiss: false,
+        };
+      }
+    }
+  }
+
+  function placeShip(ship) {
+    let direction = ['v', 'h'][Math.floor(Math.random() * 2)];
+    let max_col = 10;
+    let max_row = 10;
+    if (direction === 'v') max_row -= ship.getLength() - 1;
+    if (direction === 'h') max_col -= ship.getLength() - 1;
+    if (max_row < 1 || max_col < 1) return false;
+    let corner = [
+      Math.floor(Math.random() * max_col),
+      Math.floor(Math.random() * max_row),
+    ];
+    if (ship.canFit(board, direction, corner)) {
+      ship.insert(board, direction, corner);
+      return true;
+    } else {
+      return false;
     }
   }
 
   function placeShips() {
-    for (let i = 0; i < carrier.getLength(); i++) {
-      board[0][i].ship = carrier;
-      displayShips(board, uiBoard, board[0][i]);
+    for (let attempt = 0; attempt < 1000000; attempt++) {
+      initializeCells();
+      let attemptSuccessful = true;
+      for (let i = 0; i < ships.length; i++) {
+        if (!placeShip(ships[i])) {
+          attemptSuccessful = false;
+          break;
+        }
+      }
+      if (attemptSuccessful) {
+        console.log(this.board);
+        return;
+      }
     }
-    for (let i = 0; i < battleship.getLength(); i++) {
-      board[9][i].ship = battleship;
-      displayShips(board, uiBoard, board[9][i]);
-    }
-    for (let i = 0; i < cruiser.getLength(); i++) {
-      board[2][i].ship = cruiser;
-      displayShips(board, uiBoard, board[2][i]);
-    }
-    board[4][0].ship = destroyer1;
-    board[4][1].ship = destroyer1;
-    board[4][4].ship = destroyer2;
-    board[4][5].ship = destroyer2;
-    board[6][7].ship = submarine1;
-    board[2][8].ship = submarine2;
-    displayShips(board, uiBoard, board[4][0]);
-    displayShips(board, uiBoard, board[4][1]);
-    displayShips(board, uiBoard, board[4][4]);
-    displayShips(board, uiBoard, board[4][5]);
-    displayShips(board, uiBoard, board[6][7]);
-    displayShips(board, uiBoard, board[2][8]);
+    console.log('failed');
   }
 
   function receiveAttack(row, column) {
@@ -54,7 +75,7 @@ function Gameboard(uiBoard) {
     if (plot.ship !== null && plot.isHit === false && plot.isMiss === false) {
       plot.ship.hit();
       plot.isHit = true;
-      console.log(plot);
+      console.log('hit!', row, column);
       return true;
     } else if (
       plot.ship === null &&
@@ -62,10 +83,10 @@ function Gameboard(uiBoard) {
       plot.isMiss === false
     ) {
       plot.isMiss = true;
-      console.log(plot);
+      console.log('miss!', row, column);
       return true;
     } else if (plot.isHit === true || plot.isMiss === true) {
-      console.log('plot has already been hit');
+      console.log('plot has already been hit', row, column);
       return false;
     }
   }
@@ -86,16 +107,5 @@ function Gameboard(uiBoard) {
     }
   };
 
-  return { board, placeShips ,receiveAttack, isGameOver };
+  return { board, placeShips, receiveAttack, isGameOver };
 }
-
-function displayShips(board, uiBoard, location) {
-  const reducedBoard = board.flat();
-  let uiIndex = reducedBoard.indexOf(location);
-  uiBoard[uiIndex].style.background = 'black';
-}
-
-// function findCoordinate(board, num) {
-//   const reducedBoard = board.reduce((a, b) => a.concat(b));
-//   return reducedBoard[num];
-// }
